a0de4088588f321e20786b14b78ef180b37be9f1
fenilpanseriya2004 <fenilpanseriya2004@gmail.com>
2025-05-24 19:29:37 +0530

    change response of / route

--- controllers/Welcome.go
+++ controllers/Welcome.go
@@ -5,5 +5,5 @@ import "net/http"
 func Welcome(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "Welcome to the API!"}`))
+	w.Write([]byte(`{"message": "Welcome to the API! from of google docs"}`))
 }
